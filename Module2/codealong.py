89e108cc89b3f2c72eaf7ead3af2589a26a98a0c
Antuan Weeks <antuanweeks@gmail.com>
2016-11-06 06:52:23 -0500

    coding practice for second module

---
+++ Module2/codealong.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 17:45:05 2016
+
+@author: antuanweeks
+"""
+
+import pandas as pd
+
+path = "/Users/antuanweeks/DAT210x/Module2/Datasets/direct_marketing.csv"
+
+df = pd.read_csv(path)
+
+df.head()
