d92d30f38fd0d50dfaeb3a3068a65b51c3e999c7
Antuan Weeks <antuanweeks@gmail.com>
2016-11-06 07:24:21 -0500

    adding code from feature representation (mod2)

--- Module2/codealong.py
+++ Module2/codealong.py
@@ -12,3 +12,41 @@ path = "/Users/antuanweeks/DAT210x/Module2/Datasets/direct_marketing.csv"
 df = pd.read_csv(path)
 
 df.head()
+
+# Feature Representation
+
+income_ordered = [
+    '$1 - $1000',
+    '$1000 - $10000',
+    '$10000 - $100000',
+    '$100000 - $1000000',
+    '+$1000000']
+
+df = pd.DataFrame({
+    'income': [
+        '$100000 - $1000000',
+        '$10000 - $100000',
+        '$1 - $1000',
+        '+$1000000',
+        '$1000 - $10000',
+        '+$1000000',
+        '+$1000000',
+        '$10000 - $100000',
+        '$10000 - $100000'
+        ]
+        })
+        
+print df
+
+df.income = df.income.astype('category', 
+                             ordered=True,
+                             categories=income_ordered
+                             ).cat.codes
+
+print df
+
+df = pd.DataFrame({'vertebrates': [
+    'Bird', 
+    'Mammal', 
+    'Fish',
+    ']})
